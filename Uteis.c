41b9e51b18da9f0027a5cfc84dd26700eb390c1c
DevClaudioFilho <devclaudiofilho@gmail.com>
2024-04-16 23:30:42 +0100

    fix-Atualizacao ao template do Professor

--- Uteis.c
+++ Uteis.c
@@ -1,5 +1,29 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>//necessário p/ função time()
+
+int Aleatorio(int min, int max)
+{
+    return min + rand() % (max - min + 1);
+}
+
+int Factorial(int N)
+{
+    if (N == 0) return 1;
+    return N * Factorial(N-1);
+}
+
+int LerInteiro(char *txt)
+{
+    int x;
+    printf("%s : ", txt);
+    scanf("%d", &x);
+    return x;
+}
+
+
 #if defined(_WIN32) || defined(_WIN64)
     const char* OS = "Windows";
 #else
@@ -10,7 +34,7 @@
     #endif
 #endif
 
-void clearScreen() {
+void limparTerminal() {
     if (OS == "Windows") {
         system("cls");
     } else if (OS == "Unix") {
@@ -18,4 +42,5 @@ void clearScreen() {
     } else {
         printf("OS not supported for screen clearing.\n");
     }
+    return;
 }
--- main.c
+++ main.c
@@ -1,56 +1,78 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <conio.h>
-#include <time.h>
-#include <stdio.h>
-#include <string.h>
 
+#include "Lista.h"
+#include "Hashing.h"
+#include "Biblioteca.h"
 
-#include "users\menu_user.c"
+extern int LerInteiro(char *txt);
+//-----------------------------------------
+void Exemplo_Hashing()
+{
+    HASHING *Has = CriarHashing();
+    PESSOA *X = CriarPessoa(1234, "Jose", "CAT-A");
+    AddHashing(Has, X);
+    X = CriarPessoa(567, "Pedro", "CAT-A");
+    AddHashing(Has, X);
+    X = CriarPessoa(456, "Luis", "CAT-A");
+    AddHashing(Has, X);
+    X = CriarPessoa(56, "Miguel", "CAT-B");
+    AddHashing(Has, X);
+    X = CriarPessoa(5690, "James Bond", "CAT-Z");
+    AddHashing(Has, X);
+    ShowHashing(Has);
 
-void clearScream(){
-  printf("\33c");
+    DestruirHashing(Has);
 }
 
-// Main Menu
-int showMainMenu()
-{ 
-  int optionSelected = 0;
-  printf("-----------------------          Main Menu          -----------------------\n\n");
-  printf("[1] User menus\n");
-  printf("[0] Exit\n");
-  scanf("%d", &optionSelected);
-  return optionSelected;
+char Menu()
+{
+    limparTerminal();
+
+
+    printf("\n #----------------------------------------------------------------#");
+    printf("\n |                      Bem Vindo ao BSoft!                       |");
+    printf("\n |------------------------ Menu  Inicial -------------------------|");
+    printf("\n |                                                                |");
+    printf("\n | [1] Menu de Requisitantes                                      |");// Requisitantes
+    printf("\n | [2] Menu de Livros                                             |");// Livros
+    printf("\n | [3] Menu de Emprestimos                                        |");// Requisições
+    printf("\n | [4] Listar Biblioteca                                          |");
+    printf("\n | [5] Load Biblioteca                                            |");
+    printf("\n |                                                                |");
+    printf("\n |----------------------------------------------------------------|");
+    printf("\n | [0] Sair                                                       |");
+    printf("\n #----------------------------------------------------------------#");
+
+    char op;
+    printf("\n Selecione sua opcao: ");
+    op = getchar();
+    return op;
 }
+
 
 int main()
-{
-  printf("####################### Welcome to the HelloTeams! #######################\n");
-  int optionSelected;
-  do
-  {
-    optionSelected = showMainMenu();
-    switch (optionSelected)
+3
+    printf("Projecto-Biblioteca-Versao-Base!\n");
+    //Exemplo_Hashing();
+    BIBLIOTECA *Bib;
+    Bib = CriarBiblioteca("Biblioteca-ESTGV", "log.txt");
+    char OP;
+    do
     {
-    case 1:
-      userMain();
-      clearScream();
-      break;
-    case 0:
-      clearScream();
-      break;
-    default:
-      printf("Chose %d is not available\n", optionSelected);
-      system("pause");
-      clearScream();
-      break;
-    }
-  } while (optionSelected != 0);
+        OP = Menu();
+        switch(OP)
+        {
+            case '1': system("pause"); break;
+            case '2': system("pause"); break;
+            case '3': system("pause"); break;
+            case '4': LoadFicheiroBiblioteca(Bib); break;
+            case '5': ShowBiblioteca(Bib); break;
+            default:
+                printf("Opcao nao implementada\n"); break;
+        }
 
-  return 0;
+    }while (OP != '0');
+    DestruirBiblioteca(Bib);
+    return EXIT_SUCCESS; // ou EXIT_FAILURE
 }
